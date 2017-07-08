7f1f1f420a996de73ae1b71de4e7f41b7290286e
imamit123 <amit.k@gai.co.in>
2017-07-08 15:30:08 +0530

    Add edit item

--- src/component/todoitem.js
+++ src/component/todoitem.js
@@ -1,38 +1,78 @@
 
 import React from 'react';
 
-// class Tooditem extends React.Component {
-//  render(){
-//    return(
-//      <li onClick={() => {
-//        this.props.clickHandeler(this.props.index);
-//      }}
-//      className={
-//        this.props.detail.completed ? 'completed' : ''
-//      } >
-//      {this.props.detail.name}</li>
-//    )
-//  }
-// }
+class Todoitem extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isEditing:false
+    }
+    this.renderForm = this.renderForm.bind(this);
+    this.renderItem = this.renderItem.bind(this);
+    this.ToggalState = this.ToggalState.bind(this);
+  }
 
-const Todoitem = (props) => {
+
+renderForm(){
+ return(
+   <form>
+   <input type="text"
+     defaultValue={this.props.detail.name}
+   />
+   <button type="submit">Update</button>
+   </form>
+ )
+}
+
+ToggalState (){
+  const { isEditing } = this.state;
+  this.setState({
+    isEditing:!isEditing
+  })
+
+
+}
+
+
+renderItem(){
+ return(
+   <li onClick={ () => {
+      this.props.clickHandeler(this.props.index);
+    }}
+    className={
+      this.props.detail.completed ? 'completed' : ''
+    } >
+    {this.props.detail.name}
+    <button
+      onClick={ (event) => {
+        event.stopPropagation();
+      this.props.deleteTask(this.props.index);
+       }}>
+    Delete</button>
+    <button
+      onClick={ (event) => {
+        event.stopPropagation();
+      this.ToggalState();
+       }}>
+    Edit Item</button>
+    </li>
+ )
+
+}
+
+render(){
+//  const isEditing = this.state.isEditing;
+  const { isEditing } = this.state;
+
   return(
-    <li onClick={ () => {
-         props.clickHandeler(props.index);
-       }}
-       className={
-         props.detail.completed ? 'completed' : ''
-       } >
-       {props.detail.name}
-       <button
-         onClick={ (event) => {
-           event.stopPropagation();
-         props.deleteTask(props.index);
-          }}>
-       Delete</button>
-       </li>
+    <section>
+    {
+      isEditing ? this.renderForm() : this.renderItem()
+    }
+  </section>
 
      )
+     }
 }
 
 export default Todoitem;
